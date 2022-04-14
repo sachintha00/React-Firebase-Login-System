be1fc795eae7de4c9a8377de9673f45293a37a8e
sachintha00 <sachintha0603@gmail.com>
2022-04-14 15:38:58 +0530

    updated

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -3,17 +3,20 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import SignUp from "./components/SignUp/Signup";
+import {AuthProvider} from './contexts/AuthContext'
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <SignUp />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <SignUp />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
